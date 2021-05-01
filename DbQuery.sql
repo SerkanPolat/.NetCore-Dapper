144705c07c4a70a1dba3e9c1bdacb7f771e64e54
Serkan Polat <serkanpolat5858@hotmail.com>
2021-05-01 14:52:43 +0300

    Init

---
+++ DbQuery.sql
@@ -0,0 +1,22 @@
+﻿CREATE DATABASE DapperDB
+GO
+use DapperDB
+GO
+USE DapperDB
+GO
+CREATE TABLE Ogrenciler(
+Id int identity(1,1) primary key,
+Ad nvarchar(50)
+)
+GO
+CREATE TABLE Dersler(
+Id int identity(1,1) primary key,
+Ad nvarchar(50)
+)
+GO
+CREATE TABLE OgrenciDers(
+Id int identity(1,1) primary key,
+OgrenciId int foreign key references Ogrenciler(Id),
+DersId int foreign key references Dersler(Id)
+)
+GO
